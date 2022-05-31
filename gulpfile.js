4a07412865fe29d043206396c7d640c41263c95e
kingsleyocran <ocrankingsley@gmail.com>
2022-05-31 11:13:10 +0000

    init commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("build", () =>
-  gulp.src("css/style.css").pipe(autoprefixer()).pipe(gulp.dest("css/build"))
+  gulp.src("css/style.css")
+  .pipe(autoprefixer('last 5 versions', 'safari 5', 'ie6', 'ie7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(gulp.dest("css/build"))
 );
 
 gulp.task("watch", function () {
